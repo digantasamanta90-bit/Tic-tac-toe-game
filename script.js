cff7ec64c74e32ed35fbce903a354eb16a3e44ab
Diganta <digantasamanta90@gmail.com>
2026-07-26 03:11:42 +0530

    Refactor HTML structure and enhance game functionality with winner display and reset options

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
-let restartBtn = document.querySelector("restart");
+let restartBtn = document.querySelector(".restart");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 let turnO = true;
  const winPatterns = [
   [0, 1, 2], 
@@ -36,5 +39,56 @@ boxes.forEach((box) => {
             turnO = true;
         }
         box.disabled = true;
+        checkWinner();
     });
 });
+
+const disableBoxes = () => {
+    for (let box of boxes){
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for (let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+const checkWinner = () => {
+    for (let pattern of winPatterns){
+        // console.log(pattern[0],pattern[1],pattern[2]);
+        // console.log(
+        //     boxes[pattern[0]].innerText,
+        //     boxes[pattern[1]].innerText,
+        //     boxes[pattern[2]].innerText
+        // );
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if (pos1Val === pos2Val && pos2Val === pos3Val)  {
+                console.log("winner");
+                showWinner(pos1Val);
+            }         
+        }
+        
+    }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+restartBtn.addEventListener("click", resetGame);
+
